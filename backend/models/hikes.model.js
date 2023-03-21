637d28d02e282a048c0d56944172e4438e2a6a9b
Mateus de Oliveira <mateus.olvr@gmail.com>
2023-03-20 21:19:28 -0700

    Update: Schema

--- backend/models/hikes.model.js
+++ backend/models/hikes.model.js
@@ -4,6 +4,21 @@ import dbConnection from '../mongo/dbConnection.js';
 var accountsDb = dbConnection.useDb('articles');
 
 let schema = mongoose.Schema({
+    creationDate: String,
+    createdBy: String,
+    description: { intro: String, first: String, second: String },
+    elevation: Number,
+    lastUpdated: String,
+    length: Number,
+    location: { latitude: Number, longitude: Number, mapUrl: String },
+    routeType: String,
+    wayPoints: [
+        {
+            name: String,
+            latitude: Number,
+            longitude: Number,
+        },
+    ],
     title: String,
 });
 
