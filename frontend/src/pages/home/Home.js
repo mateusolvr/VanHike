3539f43c44762f6ffc5deec1f50251e212dd4110
souzatotolo <totolo.adv@gmail.com>
2023-03-25 19:16:51 -0700

    home screen and card component

--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,11 +1,134 @@
 import './home.css';
-import { TextField } from '@mui/material';
+import TextField from '@mui/material/TextField';
 import { Navbar } from '../../components/Navbar/Navbar';
+import Autocomplete from '@mui/material/Autocomplete';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import path from './path.png';
+import topography from './topography.png';
+import logo from '../../components/Navbar/logo.png';
+
 export const Home = () => {
+  const top100Films = [
+    { label: 'The Shawshank Redemption', year: 1994 },
+    { label: 'The Godfather', year: 1972 },
+    { label: 'The Godfather: Part II', year: 1974 },
+    { label: 'The Dark Knight', year: 2008 },
+    { label: '12 Angry Men', year: 1957 },
+    { label: "Schindler's List", year: 1993 },
+    { label: 'Pulp Fiction', year: 1994 },
+    {
+      label: 'The Lord of the Rings: The Return of the King',
+      year: 2003,
+    },
+    { label: 'The Good, the Bad and the Ugly', year: 1966 },
+    { label: 'Fight Club', year: 1999 },
+    {
+      label: 'The Lord of the Rings: The Fellowship of the Ring',
+      year: 2001,
+    },
+  ];
   return (
     <div>
       <Navbar />
-      <h1>Home</h1>
+      <div className="mainContainer">
+        <h2 className="mainPageh2">Time to go to your own</h2>
+        <h3 className="mainPageh3"> Adventure</h3>
+        <div className="filterContainer">
+          <Autocomplete
+            disablePortal
+            id="combo-box-demo"
+            options={top100Films}
+            sx={{ width: 300, color: '#FFFFFF' }}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Location"
+                InputLabelProps={{
+                  style: {
+                    color: '#FFF',
+                    fontWeight: '400',
+                    fontFamily: 'Montserrat',
+                  },
+                }}
+                sx={{
+                  borderRight: 'solid 2px #fff',
+                  marginBottom: '10px',
+                  color: 'transparent',
+                }}
+              />
+            )}
+          />
+
+          <Autocomplete
+            disablePortal
+            id="combo-box-demo"
+            options={top100Films}
+            sx={{ width: 300 }}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Terrain"
+                InputLabelProps={{
+                  style: {
+                    color: '#FFF',
+                    fontWeight: '400',
+                    fontFamily: 'Montserrat',
+                  },
+                }}
+                sx={{
+                  borderRight: 'solid 2px #fff',
+                  marginBottom: '10px',
+                  color: 'transparent',
+                }}
+              />
+            )}
+          />
+
+          <Autocomplete
+            disablePortal
+            id="combo-box-demo"
+            options={top100Films}
+            sx={{ width: 300 }}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Difficulty"
+                InputLabelProps={{
+                  style: {
+                    color: '#FFF',
+                    fontWeight: '400',
+                    fontFamily: 'Montserrat',
+                  },
+                }}
+                sx={{
+                  marginBottom: '10px',
+                  color: 'transparent',
+                  border: 'none',
+                  '&:focus': {
+                    borderColor: '#fff',
+                  },
+                }}
+              />
+            )}
+          />
+        </div>
+        <Card />
+        {/* <img src={topography} className="topography" /> */}
+      </div>
+      <footer className="footer">
+        <img className="footerLogo" src={logo} alt="logo" />
+        <span>2023 VanHike - All rights reserved</span>
+        <div className="connectSmallDiv">
+          <p>Connect with us</p>
+          <p>F</p>
+          <p>I</p>
+        </div>
+      </footer>
     </div>
   );
 };
