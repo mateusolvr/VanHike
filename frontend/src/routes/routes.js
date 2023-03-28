1662def05ec870a2875e5e8d838bd6680bfce27c
Vinícius Watanabe <vinicius_watanabe@hotmail.com>
2023-03-27 22:44:06 -0700

    finished admin login page

--- frontend/src/routes/routes.js
+++ frontend/src/routes/routes.js
@@ -2,13 +2,15 @@ import { Routes, Route } from 'react-router-dom';
 import { ErrorPage } from '../pages/404/ErrorPage';
 import { Home } from '../pages/home/Home';
 import { Trail } from '../pages/trail/Trail';
+import { Admin } from '../pages/admin/Admin';
 
 export const AppRoutes = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/trails/1" element={<Trail />} />
-      <Route path="*" element={<ErrorPage />} />
-    </Routes>
-  );
+	return (
+		<Routes>
+			<Route path='/' element={<Home />} />
+			<Route path='/trails/1' element={<Trail />} />
+			<Route path='/admin' element={<Admin />} />
+			<Route path='*' element={<ErrorPage />} />
+		</Routes>
+	);
 };
