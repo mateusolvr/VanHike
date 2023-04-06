e6f5b4eb9d249aace0784e6594c860ebe5d34b12
Mateus de Oliveira <mateus.olvr@gmail.com>
2023-04-06 11:20:14 -0700

    Add: authorization & users endpoints

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,18 @@ import express from 'express';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 // import dbConnection from './mongo/dbConnection.js';
-import router from './routes/routes.js';
+import routes from './routes/routes.js';
+import userRoutes from './routes/users.js';
+import authRoutes from './routes/auth.js';
 
 dotenv.config();
 
 const app = express();
 app.use(bodyParser.json());
 
-app.use('/vanhike', router);
+app.use('/vanhike', routes);
+app.use('/users', userRoutes);
+app.use('/auth', authRoutes);
 
 const APP_PORT = process.env.PORT || 5000;
 app.listen(APP_PORT, () => {
