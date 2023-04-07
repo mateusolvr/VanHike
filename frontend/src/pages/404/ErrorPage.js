b4ba9ce03353dc677158be37f1483535c49075df
souzatotolo <totolo.adv@gmail.com>
2023-04-07 16:25:03 -0700

    implemented error page

--- frontend/src/pages/404/ErrorPage.js
+++ frontend/src/pages/404/ErrorPage.js
@@ -1,8 +1,16 @@
+import './error.css';
+import { Link } from 'react-router-dom';
+import { IoReturnDownBack } from 'react-icons/io5';
+
 export const ErrorPage = () => {
   return (
-    <div>
+    <div className="errorContainer">
       <h2>404</h2>
       <p>SORRY, PAGE NOT FOUND</p>
+      <Link to="/">
+        {' '}
+        <IoReturnDownBack size={30} />
+      </Link>
     </div>
   );
 };
