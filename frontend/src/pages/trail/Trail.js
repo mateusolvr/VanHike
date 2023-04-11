714fdc04381141ffc4cfa25ed415e47d8fc78689
souzatotolo <totolo.adv@gmail.com>
2023-04-10 19:36:31 -0700

    single trail fetch fix

--- frontend/src/pages/trail/Trail.js
+++ frontend/src/pages/trail/Trail.js
@@ -23,7 +23,7 @@ export const Trail = () => {
   useEffect(() => {
     const getTrailData = async () => {
       await axios
-        .get(`http://localhost:8005/vanhike/hikes/642f140b5af6de438e7b5a91`)
+        .get(`http://localhost:8005/vanhike/hikes/${_id}`)
         .then((res) => {
           setTrail(res.data);
           console.log(res.data);
@@ -33,13 +33,19 @@ export const Trail = () => {
         });
     };
     getTrailData();
-  }, []);
+  }, [_id]);
 
   return (
     <div>
       <Navbar />
       <div className="singleTrailContainer">
-        <img className="singleTrailImg" src={trail.images.main} alt="" />
+        <img
+          className="singleTrailImg"
+          src={
+            trail && trail.images && trail.images.main ? trail.images.main : ''
+          }
+          alt=""
+        />
         <div className="singleTrailInfoContainer">
           <div className="miniWeatherContainer">
             <div>
@@ -55,11 +61,10 @@ export const Trail = () => {
           <p className="singleTrailCoordinates">50.324123 N, 122.213123 W</p>
           <div className="smallDiv"></div>
           <p className="singleTrailDescription">
-            Joffre Lakes in British Columbia, Canada, features three stunning
-            glacial-fed lakes surrounded by towering mountains and lush forests.
-            The scenic trail through the wilderness offers a chance to
-            experience the natural beauty of the area up close, making it a
-            must-see destination for nature lovers and hikers alike.
+            {' '}
+            {trail && trail.description && trail.description.intro
+              ? trail.description.intro
+              : ''}
           </p>
 
           <div className="map">
@@ -92,64 +97,97 @@ export const Trail = () => {
         </div>
         <div className="picAndTextContainer">
           <p>
-            Joffre Lakes is a true gem of British Columbia, located in the heart
-            of the breathtaking Pemberton Valley. This stunning hiking
-            destination is known for its three turquoise glacial-fed lakes that
-            are a sight to behold. The trail to the lakes is a 10 km journey
-            through lush forests, meadows, and up to the majestic Upper Joffre
-            Lake. Each lake offers something special, from Lower Joffre Lake's
-            tranquil waters, to Middle Joffre Lake's panoramic views of the
-            surrounding mountains, to Upper Joffre Lake's awe-inspiring sight of
-            the Matier Glacier towering above it. Joffre Lakes Provincial Park
-            is a paradise for nature lovers, hikers, and photographers. Whether
-            you're seeking adventure or relaxation, the natural beauty of Joffre
-            Lakes is an experience that will leave you breathless. Aside from
-            its mesmerizing lakes, Joffre Lakes Provincial Park offers plenty of
-            opportunities for outdoor activities. Visitors can go fishing or
-            kayaking on the lakes, or explore the nearby glaciers and
-            waterfalls. The park also features a campground where visitors can
-            enjoy a night under the stars surrounded by nature.
+            {trail && trail.description && trail.description.first
+              ? trail.description.first
+              : ''}
           </p>
-          <img src={trail.images.main} alt="" />
+          <img
+            src={
+              trail && trail.images && trail.images.first
+                ? trail.images.first
+                : ''
+            }
+            alt=""
+          />
         </div>
         <div className="picAndTextContainer2">
-          <img src={trail.images.main} alt="" />
+          <img
+            src={
+              trail && trail.images && trail.images.second
+                ? trail.images.second
+                : ''
+            }
+            alt=""
+          />
           <p>
-            The trail to Joffre Lakes is considered moderately difficult, but
-            the breathtaking views and serene ambiance are worth the effort.
-            Hiking the trail during the fall season offers visitors the chance
-            to witness the beautiful autumn colors that envelop the surrounding
-            forests. Overall, Joffre Lakes is a paradise for outdoor enthusiasts
-            and nature lovers. Its stunning glacial-fed lakes, towering
-            mountains, and lush forests create an unparalleled landscape that
-            must be seen to be believed. A trip to Joffre Lakes is a
-            once-in-a-lifetime experience that will leave you with unforgettable
-            memories.
+            {trail && trail.description && trail.description.second
+              ? trail.description.second
+              : ''}
           </p>
         </div>
       </div>
-      <img className="horizontalImg" src={trail.images.main} alt="" />
+      <img
+        className="horizontalImg"
+        src={
+          trail && trail.images && trail.images.footer
+            ? trail.images.footer
+            : ''
+        }
+        alt=""
+      />
       <div className="weatherContainer">
         <h2>Weather</h2>
         <div className="horizontalLine"></div>
         <div className="weatherListContainer">
           <ul>
             {daysOfWeek.map((day) => (
-              <>
+              <div className="weatherMiniDiv">
                 <p>{day}</p>
-              </>
+                <BsCloudSnow size={30} />
+                <p className="weatherMiniDivForecast">Snowing</p>
+                <p className="weatherMiniDivForecast">H:7°C L:-2°C</p>
+              </div>
             ))}
           </ul>
         </div>
-        <div className="waypointsDiv">
+        {/* <div className="waypointsDiv">
           <h2>Way points</h2>
           <div className="horizontalLine"></div>
           <div className="waypointsListContainer">
-            <ul>
-              <li>Waypoints</li>
-            </ul>
+            <div className="waypoint">
+              <p className="waypointNumber">1</p>
+              <p>
+                {trail && trail.wayPoints && trail.wayPoints[0].name
+                  ? trail.wayPoints[0].name
+                  : ''}
+              </p>
+            </div>
+            <div className="waypoint">
+              <p className="waypointNumber">2</p>
+              <p>
+                {trail && trail.wayPoints && trail.wayPoints.name[0]
+                  ? trail.wayPoints.name[0]
+                  : ''}
+              </p>
+            </div>
+            <div className="waypoint">
+              <p className="waypointNumber">3</p>
+              <p>
+                {trail && trail.images && trail.images.footer
+                  ? trail.images.footer
+                  : ''}
+              </p>
+            </div>
+            <div className="waypoint">
+              <p className="waypointNumber">4</p>
+              <p>
+                {trail && trail.wayPoints && trail.wayPoints.name[0]
+                  ? trail.wayPoints.name[0]
+                  : ''}
+              </p>
+            </div>
           </div>
-        </div>
+        </div> */}
       </div>
       <footer className="footer">
         <img className="footerLogo" src={logo} alt="logo" />
